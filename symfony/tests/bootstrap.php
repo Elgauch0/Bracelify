4f7b337f00add2283f606323a23df57013bb3e55
El Kaouri Anass <elkaourianass@gmail.com>
2026-05-16 19:10:27 +0200

    test(env): add automated test DB reset in bootstrap and cleanup test compose

--- symfony/tests/bootstrap.php
+++ symfony/tests/bootstrap.php
@@ -5,9 +5,18 @@ use Symfony\Component\Dotenv\Dotenv;
 require dirname(__DIR__).'/vendor/autoload.php';
 
 if (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env.test');
 }
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 }
+
+$_SERVER['APP_ENV'] = 'test';
+$_ENV['APP_ENV'] = 'test';
+$_SERVER['APP_DEBUG'] = '0';
+
+// On recrée la base test à chaque run
+passthru('php bin/console doctrine:database:drop --force --env=test --if-exists');
+passthru('php bin/console doctrine:database:create --env=test');
+passthru('php bin/console doctrine:migrations:migrate --no-interaction --env=test');
